4eb86a668f4394d877ca5e11185de0c570792205
Guest <guest-55EJSZ@meet.mit.edu>
2014-12-15 20:03:26 +0200

    project

---
+++ meet-django/pollingsite/polls/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models(classes) here.
+class Poll(models.Model):
+	question = models.CharField(max_length=200)
+
+class Choice(models.Model):
+	choice_text = models.CharField(max_length=30)
+	votes = models.IntegerField()
+	poll = models.ForeignKey(Poll)
